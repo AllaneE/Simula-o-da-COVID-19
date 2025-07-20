eb691d94e27b9449a9be143ee5368eb6ab22005b
Allane Emanoelle <98751443+AllaneE@users.noreply.github.com>
2025-07-20 00:55:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,6 @@ for node, data in G_original.nodes(data=True):
     net.add_node(node, label=label, color="#C6E5B1", title=f"<b>{label}</b><br>Status: Desconhecido", size=10)
 for source, target in G_original.edges():
     net.add_edge(source, target)
-net.set_options('{"physics": {"enabled": false}}')
 components.html(net.generate_html(), height=800)
 
 # Exibir métricas do grafo SEIR
@@ -63,7 +62,6 @@ for node, data in G_seir.nodes(data=True):
     label = f"Nó {node}"
     net.add_node(node, label=label, color=color, title=f"<b>{label}</b><br>Status: {status}", size=10)
 for source, target in G_seir.edges():
-net.set_options('{"physics": {"enabled": false}}')
 components.html(net.generate_html(), height=800)
 
 # Top 10 nós mais infectados
@@ -87,7 +85,6 @@ for node, data in G_risco.nodes(data=True):
     net.add_node(node, label=label, color=color, title=f"<b>{label}</b><br>Status: {status}", size=10)
 for source, target in G_risco.edges():
     net.add_edge(source, target)
-net.set_options('{"physics": {"enabled": false}}')
 components.html(net.generate_html(), height=800)
 
 # Legenda
